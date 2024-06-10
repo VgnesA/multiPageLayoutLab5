8a0ea75da8c7290edcfa36e49daaeb9ef5832b1e
Vignesh Alle <134603954+VgnesA@users.noreply.github.com>
2024-06-09 23:41:14 -0400

    Add files via upload

---
+++ multiPageLayoutLab5/multiPageLayoutLab5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  multiPageLayoutLab5
+//
+//  Created by user256361 on 6/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
